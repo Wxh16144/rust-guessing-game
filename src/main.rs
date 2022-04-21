b128a2d7874747eda3e7358bd18b900c657e68ef
wuxh <wxh1220@gmail.com>
2022-04-21 21:33:55 +0800

    chore: 只执行一次猜测
    https://www.bilibili.com/video/BV1hp4y1k7SV?p=5

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("猜数字");
+
+    println!("请输入一个数字");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("无法读取行");
+
+    println!("您猜测的数字是:{}", guess);
 }
