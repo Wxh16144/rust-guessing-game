944dafdf153153535b9213894007f7fb54022e99
wuxh <wxh1220@gmail.com>
2022-04-21 22:06:53 +0800

    feat: 实现多次猜测功能
    https://www.bilibili.com/video/BV1hp4y1k7SV?p=8

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,35 @@
 use rand::Rng;
-use std::io;
 use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("猜数字");
 
-    println!("请输入一个数字");
-
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("神秘数字是:{}", secret_number);
+    // println!("神秘数字是:{}", secret_number);
 
-    let mut guess = String::new();
+    loop {
+        println!("请输入一个数字");
 
-    io::stdin().read_line(&mut guess).expect("无法读取行");
+        let mut guess = String::new();
 
-    let guess:u32 = guess.trim().parse().expect("请输入数字");
+        io::stdin().read_line(&mut guess).expect("无法读取行");
 
-    match guess.cmp(&secret_number){
-        Ordering::Less => println!("太小了"),
-        Ordering::Greater => println!("太大了"),
-        Ordering::Equal => println!("猜对了"),
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("您猜测的数字是:{}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("太小了"),
+            Ordering::Greater => println!("太大了"),
+            Ordering::Equal => {
+                println!("猜对了");
+                break;
+            }
+        }
     }
-
-    println!("您猜测的数字是:{}", guess);
 }
